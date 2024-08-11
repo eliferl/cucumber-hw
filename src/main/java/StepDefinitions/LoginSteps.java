1a3b84cded0f22e8a849b814d038426232c164b0
eliferl <eellif96@gmail.com>
2024-08-11 18:16:49 +0300

    util paketine tasindi

--- src/main/java/StepDefinitions/LoginSteps.java
+++ src/main/java/StepDefinitions/LoginSteps.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pages.DashboardPage;
-import pages.DriverUtil;
+import utils.DriverUtil;
 import pages.LoginPage;
 
 public class LoginSteps {
--- src/main/java/StepDefinitions/SearchSteps.java
+++ src/main/java/StepDefinitions/SearchSteps.java
@@ -4,13 +4,9 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import pages.DashboardPage;
-import pages.DriverUtil;
-import pages.LoginPage;
+import utils.DriverUtil;
 import pages.SearchPage;
-
-import java.time.Duration;
 
 public class SearchSteps {
 
